171dfa5103c94e441c7e4ed3256f0ccbaf9071d0
muthu4098 <muthu4098@gmail.com>
2025-08-01 13:13:59 +0530

    movie-Task

---
+++ src/User.jsx
@@ -0,0 +1,11 @@
+
+export function User({ pic, name }) {
+  return (
+    <div className="container">
+      <img src={pic} alt={$} {...name} profile is not found />} />
+      <p>
+        Hello, <span>{name}</span>🙂‍↕
+      </p>
+    </div>
+  );
+}
--- src/reatnote.md
+++ src/reatnote.md
@@ -7,4 +7,10 @@ backward compataable
 git init -intall
 eslint -Team force use style
 
+## two type
+named export
+default export- one one per 
+file
+rename as rename variable
 
+one delete ->ctrl + space
