a214db826a352e4d3953fa913c7108e2bef95c52
muthu4098 <muthu4098@gmail.com>
2025-08-02 12:52:41 +0530

    like task

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
+import { Counter } from "./Counter";
 import { MovieList } from "./MovieList";
 import "./styles.css";
 import { UserList } from "./UserList";
+
 
 //Component
 export default function App() {
   return (
     <div className="App">
        {/* <UserList />  */}
-      <MovieList />
+      {/* <MovieList /> */}
+      {/* <Counter /> */}
     </div>
   );
 }
