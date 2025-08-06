c0cd21e12747392bf0901326bed8de1eaecda60e
muthu4098 <muthu4098@gmail.com>
2025-08-06 11:09:15 +0530

    progress task done

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,13 @@ import { MovieList } from "./MovieList";
 import "./styles.css";
 import { UserList } from "./UserList";
 
-
 //Component
 export default function App() {
   return (
     <div className="App">
        {/* <UserList />  */}
       {/* <MovieList /> */}
-      {/* <Counter /> */}
+      <Counter />
     </div>
   );
 }
--- src/Counter.jsx
+++ src/Counter.jsx
@@ -2,9 +2,10 @@ import { useState } from "react";
 
 export function 
 Counter() {
-  const [like, setLike] = useState(10);
-  const [disLike, setLike1] = useState(10);
+  const [like, setLike] = useState(0);
+  const [disLike, setLike1] = useState(0);
 
+  
 
   // Dislike
   return (
@@ -12,8 +13,12 @@ Counter() {
       <button onClick={() => setLike(like + 1)}>👍 {like}</button>
 
       <button onClick={() => setLike1(disLike + 1)}>👎 {disLike}</button>
-      <progress max={like + disLike} value={like}></progress>
+      <progress max={like + disLike} value={like} ></progress>
+      {/* conditional Rendering */}
+      {like-disLike>10 ? <h1>Awesome</h1> :<h1></h1>}
+      {like>=disLike-10 ? <h1>Awesome</h1> :<h1></h1>}
 
     </div>
   );
 }
+
